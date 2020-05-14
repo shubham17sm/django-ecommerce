31250cd4add80bf545cc7962a00f98971f6392eb
shubham <shubham17.sm@gmail.com>
2020-05-14 16:12:07 +0530

    admin customization

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Item, OrderItem, Order, BilingAddress, UserProfile
 # Register your models here.
-admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
-admin.site.register(BilingAddress)
 admin.site.register(UserProfile)
+
+
+class BilingAddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'country', 'zipcode', 'address_type')
+
+admin.site.register(BilingAddress, BilingAddressAdmin)
+
+
+class ItemDisplayAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'list_on_frontpage')
+    list_filter = ('category',)
+    search_fields = ('title', 'category', 'description', 'slug')
+
+admin.site.register(Item, ItemDisplayAdmin)
